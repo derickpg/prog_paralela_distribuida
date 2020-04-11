43611e35935d2c3abc51426a1c733eb806d33de8
derickpg <garceczderick@gmail.com>
2020-04-11 16:13:05 -0300

    Incluindo mais metodos no Servidor

--- servidor.c
+++ servidor.c
@@ -1,5 +1,3 @@
-/* servidor.c (Roland Teodorowitsch; 30 ago. 2013 - 31 mar. 2017) */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,6 +18,7 @@ CONTAS lista_contas[limite_contas] = {
 	{"13201878", 20.0}
 };
 
+// Consulta Saldo
 double *obtem_saldo_conta_1_svc(char **num_conta)
 {
 
@@ -32,6 +31,8 @@ double *obtem_saldo_conta_1_svc(char **num_conta)
 	return &erro;
 }
 
+
+// Verifica se a conta existe
 bool *obtem_conta_1_svc(char **num_conta)
 {
 	int i;
@@ -42,9 +43,37 @@ bool *obtem_conta_1_svc(char **num_conta)
 	return false;
 }
 
+// Deposito
+bool *deposito_conta_1_svc(char **num_conta, double **valor)
+{
+	int i;
+
+	for	(i=0;i<qnt_contas;++i)
+		if	(strcmp(lista_contas[i].num_conta,*num_conta)==0)
+			*(lista_contas[i].saldo) = (&(lista_contas[i].saldo) + *valor) ; //Adiciona o valor do deposito na conta
+			return true;
+	return false;
+}
+
+// Saque
+bool *deposito_conta_1_svc(char **num_conta, double **valor)
+{
+	int i;
+
+	for	(i=0;i<qnt_contas;++i)
+		if	(strcmp(lista_contas[i].num_conta,*num_conta)==0)
+			*(lista_contas[i].saldo) = (&(lista_contas[i].saldo) - *valor) ; //Adiciona o valor do deposito na conta
+			return true;
+	return false;
+}
+
+
+
+
 
 int main()
 {
+	//copia do exemplo, nao sei como configurar !
 	if	(registerrpc(NOTASPROG,NOTASVERS,OBTEM_NOTA,obtem_nota_1_svc,
 			     (xdrproc_t)xdr_wrapstring,(xdrproc_t)xdr_double ) == -1)  {
 		fprintf(stderr,"Erro em registerrpc()!\n");
